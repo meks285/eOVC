234a399f6c8df1a2e6d94a5833d1c865c4a3c6da
meks285 <meks285@yahoo.com>
2021-02-10 14:41:11 +0100

    Complete VH

--- src/app/pages/vulnerablehouseholdref/vulnerablehouseholdref.page.ts
+++ src/app/pages/vulnerablehouseholdref/vulnerablehouseholdref.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
+import { AlertController, LoadingController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-vulnerablehouseholdref',
@@ -6,10 +8,128 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vulnerablehouseholdref.page.scss'],
 })
 export class VulnerablehouseholdrefPage implements OnInit {
+  state: any;
+  lga: any;
+  community: any;
+  name_of_referred: any;
+  phone: any;
+  sex: any;
+  age_yrs: any;
+  unique_id: any;
+  address: any;
+  referring_cbo_name: any;
+  receiving_cbo_name: any;
+  service1: any;
+  service2: any;
+  service3: any;
+  service4: any;
+  service5: any;
+  service6: any;
+  service7: any;
+  service8: any;
+  service9: any;
+  service10: any;
+  service11: any;
+  service12: any;
+  service13: any;
+  service14: any;
+  service15: any;
+  service16: any;
+  service17: any;
+  service18: any;
+  service19: any;
+  service20: any;
+  service21: any;
+  service22: any;
+  service23: any;
+  service24: any;
+  service25: any;
+  service26: any;
+  service27: any;
+  service28: any;
+  service29: any;
+  service30: any;
+  service31: any;
+  service32: any;
+  additionalcomments: any;
+  referrers_name: any;
+  designation: any;
+  phoneno_email: any;
+  initials: any;
+  org_prov_referral: any;
+  list_service_1: any;
+  list_service_1_completed: any;
+  list_service_1_followup: any;
+  list_service_1_date: any;
+  list_service_2: any;
+  list_service_2_completed: any;
+  list_service_2_followup: any;
+  list_service_2_date: any;
+  additionalcomments_section_b: any;
+  referral_complete: any;
+  name_of_referral_receiver: any;
+  designation_of_referral_receiver: any;
+  phoneno_of_referral_receiver: any;
+  initials_of_referral_receiver: any;
 
-  constructor() { }
+  constructor(private sqlite: SQLite, private alertController: AlertController, private navController: NavController, private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
   }
-
+  async submit(){
+    const loading = await this.loadingCtrl.create({
+      message: 'Submitting...',
+    });
+  await loading.present();
+  if(this.state==undefined||this.lga==undefined||this.community==undefined||this.referring_cbo_name==undefined||this.receiving_cbo_name==undefined||this.unique_id==undefined||this.designation==undefined||this.referrers_name==undefined||this.designation==undefined||this.phoneno_email==undefined||this.phoneno_email==undefined||this.name_of_referral_receiver==undefined){
+    this.loadingCtrl.dismiss();
+      const alert = this.alertController.create({
+        message: 'Incomplete VHReferral Form',
+        buttons: ['OK']
+      }).then((alert)=>{
+        alert.present();
+      });
+    }
+    else{
+      this.sqlite.create({
+        name: 'eovc.db',
+        location: 'default'
+      }).then((db: SQLiteObject) => {
+        db.executeSql('INSERT INTO vulnerablehousehold VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',[null,this.state,this.lga,this.community,this.name_of_referred,this.phone,this.sex,this.age_yrs,this.unique_id,this.address,this.referring_cbo_name,this.receiving_cbo_name,this.service1,this.service2,this.service3,this.service4,this.service5,this.service6,this.service7,this.service8,this.service9,this.service10,this.service11,this.service12,this.service13,this.service14,this.service15,this.service16,this.service17,this.service18,this.service19,this.service20,this.service21,this.service22,this.service23,this.service24,this.service25,this.service26,this.service27,this.service28,this.service29,this.service30,this.service31,this.service32,this.additionalcomments,this.referrers_name,this.designation,this.phoneno_email,this.initials,this.org_prov_referral,this.list_service_1,this.list_service_1_completed,this.list_service_1_followup,this.list_service_1_date,this.list_service_2,this.list_service_2_completed,this.list_service_2_followup,this.list_service_2_date,this.additionalcomments_section_b,this.referral_complete,this.name_of_referral_receiver,this.designation_of_referral_receiver,this.phoneno_of_referral_receiver,this.initials_of_referral_receiver])
+          .then(res => {
+            this.loadingCtrl.dismiss();
+            console.log(res);
+            const alert = this.alertController.create({
+              message: 'Vulnerable Household Referral submitted',
+              buttons: [{
+                text: 'Continue',
+                role: 'destructive',
+                handler: () => {
+                  this.navController.navigateBack('');
+                }
+              }]
+            }).then((alert)=>{
+              alert.present();
+            });
+          })
+          .catch(e => {
+            this.loadingCtrl.dismiss();
+            const alert = this.alertController.create({
+              message: 'Vulnerable Household NOT Referral Submitted'+e.message,
+              buttons: [{
+                text: 'OK',
+                role: 'destructive',
+                handler: () => {
+                  //navigator['app'].ExitApp();
+                }
+              }]
+            }).then((alert)=>{
+              alert.present();
+            });
+          });
+      }).catch(e => {
+        console.log(e);
+      });
+    }
+  }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -60,6 +60,10 @@ export class Tab1Page {
         .then(res => {
           console.log('Vulnerable Children Table Created');
         }).catch(e => console.log(e));
+        db.executeSql('CREATE TABLE IF NOT EXISTS vulnerablehousehold(id INTEGER PRIMARY KEY AUTOINCREMENT,state TEXT,lga TEXT,community TEXT,name_of_referred TEXT,phone TEXT,sex TEXT,age_yrs TEXT,unique_id TEXT,address TEXT,referring_cbo_name TEXT,receiving_cbo_name TEXT,service1 TEXT,service2 TEXT,service3 TEXT,service4 TEXT,service5 TEXT,service6 TEXT,service7 TEXT,service8 TEXT,service9 TEXT,service10 TEXT,service11 TEXT,service12 TEXT,service13 TEXT,service14 TEXT,service15 TEXT,service16 TEXT,service17 TEXT,service18 TEXT,service19 TEXT,service20 TEXT,service21 TEXT,service22 TEXT,service23 TEXT,service24 TEXT,service25 TEXT,service26 TEXT,service27 TEXT,service28 TEXT,service29 TEXT,service30 TEXT,service31 TEXT,service32 TEXT,additionalcomments TEXT,referrers_name TEXT,designation TEXT,phoneno_email TEXT,initials TEXT,org_prov_referral TEXT,list_service_1 TEXT,list_service_1_completed TEXT,list_service_1_followup TEXT,list_service_1_date TEXT,list_service_2 TEXT,list_service_2_completed TEXT,list_service_2_followup TEXT,list_service_2_date TEXT,additionalcomments_section_b TEXT,referral_complete TEXT,name_of_referral_receiver TEXT,designation_of_referral_receiver TEXT,phoneno_of_referral_receiver TEXT,initials_of_referral_receiver TEXT)', [])
+        .then(res => {
+          console.log('Vulnerable Household Table Created');
+        }).catch(e => console.log(e));
       })
       .catch(e => console.log(e));
   }
